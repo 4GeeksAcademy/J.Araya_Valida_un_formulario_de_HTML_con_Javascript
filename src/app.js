e9db4bb08c921d63914b3ce1924ca904792c6a12
ImJAraya <fabiarayamontero@gmail.com>
2025-03-27 14:56:42 -0600

    completo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,43 @@
-import "bootstrap";
-import "./style.css";
+const ids = ["CVC-#"
+  , "Card-#",
+  "Amount",
+  "First-Name",
+  "Last-Name",
+  "City",
+  "State",
+  "Mensaje",
+  "Postal_code"];
 
+document.getElementById("submit").addEventListener("click", function (e) {
+  e.preventDefault();
+  validar()
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+})
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function validar() {
+  let todosLlenos = ids.every(id => document.getElementById(id).value.trim() !== "");
+  let metDePagoMarcado = document.querySelector('input[name="Payment"]:checked')
+  let estado = document.getElementById("State").value
+  console.log(todosLlenos, metDePagoMarcado, estado)
+
+  if (!todosLlenos || metDePagoMarcado === null) {
+    
+    let alert = document.getElementById("alert")   //listooooo
+    alert.classList.add("alert", "alert-danger");  //listooooo
+    alert.innerHTML = "Some fields are missing"     //listooooo
+
+    let vacios = ids.filter(id => {
+      let ele = document.getElementById(id);
+      return !ele || ele.value.trim() === "" || estado === "Pick a state";
+    });
+    for (const id of vacios) {
+      document.getElementById(id).classList.add("alert", "alert-danger");
+      if (metDePagoMarcado === null){
+        document.getElementById("metodosDePago").classList.add("alert", "alert-danger");
+
+      }
+    }
+    
+  }
+
+}
