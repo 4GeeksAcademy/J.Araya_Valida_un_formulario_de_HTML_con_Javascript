425af6828fa4aa9cf08df3f1d567607f24a0fe8f
ImJAraya <fabiarayamontero@gmail.com>
2025-03-27 15:41:46 -0600

    ya quita el cuadro rojo al llnar las casillas

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ const ids = ["CVC-#"
   "First-Name",
   "Last-Name",
   "City",
-  "State",
   "Mensaje",
   "Postal_code"];
 
@@ -17,27 +16,43 @@ document.getElementById("submit").addEventListener("click", function (e) {
 function validar() {
   let todosLlenos = ids.every(id => document.getElementById(id).value.trim() !== "");
   let metDePagoMarcado = document.querySelector('input[name="Payment"]:checked')
-  let estado = document.getElementById("State").value
-  console.log(todosLlenos, metDePagoMarcado, estado)
+  let estado = document.getElementById("State")
+ 
+  //--------------------------LIMPIAR CASILLAS----------------------------------------------
+  for (const id of ids) {
+    document.getElementById(id).classList.remove("alert", "alert-danger")
+    document.getElementById("metodosDePago").classList.remove("alert", "alert-danger");
+  }
 
-  if (!todosLlenos || metDePagoMarcado === null) {
-    
+  estado.classList.remove("is-invalid")
+
+
+  let alert = document.getElementById("alert")   //listooooo
+  alert.classList.remove("alert", "alert-danger");  //listooooo
+  alert.innerHTML = ""     //listooooo
+
+  //----------------------FIN LIMPIAR CASILLAS----------------------------------------------
+
+
+
+  if (!todosLlenos || metDePagoMarcado === null || estado.value === "Pick a state") {
+
     let alert = document.getElementById("alert")   //listooooo
     alert.classList.add("alert", "alert-danger");  //listooooo
     alert.innerHTML = "Some fields are missing"     //listooooo
 
     let vacios = ids.filter(id => {
       let ele = document.getElementById(id);
-      return !ele || ele.value.trim() === "" || estado === "Pick a state";
+      return !ele || ele.value.trim() === "";
     });
     for (const id of vacios) {
       document.getElementById(id).classList.add("alert", "alert-danger");
-      if (metDePagoMarcado === null){
-        document.getElementById("metodosDePago").classList.add("alert", "alert-danger");
-
-      }
     }
-    
+    if (metDePagoMarcado === null){
+      document.getElementById("metodosDePago").classList.add("alert", "alert-danger");
+    }
+    if (estado.value === "Pick a state") {
+      estado.classList.add("is-invalid");
+    }
   }
-
 }
